75b76710216268df7b897bf610235c3c447f2739
Tan Nguyen <nguyenthanhtan090102@gmail.com>
2025-04-25 21:34:10 -0500

    ENH: Add tests to the app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,29 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
+import { By } from "@angular/platform-browser";
+import { ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>
+  let component: AppComponent
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
+      imports: [
+        AppComponent
+      ],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(AppComponent)
+      component = fixture.componentInstance
+    });
   });
 
   it('should create the app', () => {
@@ -24,6 +42,33 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, placeholder-text-creation');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Random Paragraph Generator');
   });
+
+  it('should render input field', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('input')).toBeTruthy()
+  })
+
+  it('should render "Generate" button', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('button')?.textContent).toEqual('Generate')
+  })
+
+  it('should call the onGenerateParagraph function when all valid', fakeAsync(() => {
+    spyOn(component, 'onGenerateParagraph')
+    
+    const inputField = fixture.debugElement.query(By.css('#numParagraphInput')).nativeElement
+    const button = fixture.debugElement.nativeElement.querySelector('button')
+
+    inputField.value = "1"
+    button?.click()
+    tick();
+
+    expect(component.onGenerateParagraph).toHaveBeenCalled()
+  }));
 });
